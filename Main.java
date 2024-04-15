55a7d2ac57402ce57b5cb20efe82846499e7523b
S. K. GUPTA <122195191+soumyakumargupta@users.noreply.github.com>
2024-04-15 15:02:28 +0530

    Add files via upload
    Solution of Question 2

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Food cost problem 2 solution
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        System.out.println(6*x + y);
+    }
+}
